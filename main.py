64956d5c8a35626761132ae5688bd29e6fc1da27
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-12 21:03:46 +0700

    Create main.py
    program utama untuk mengumpulkan fungsi2 dari spek

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import math
+import time
+import argparse
+import datetime
+import csv
+
+from Fungsi-Utama import *
+list_users = convertuser("user.csv")
+list_candi = convertcandi("candi.csv")
+list_bahan_bangunan = convertbb("bahan_bangunan.csv")
