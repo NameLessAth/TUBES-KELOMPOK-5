2b5cb01b3f4b9f79b2998d85305b5208aaf933ab
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-15 01:16:11 +0700

    penambahan F09 ke program main.py
    penambahan F09 yang telah lengkap ke main.py

--- main.py
+++ main.py
@@ -4,9 +4,33 @@ import math
 import time
 import argparse
 import datetime
-import csv
 
 from Fungsi-Utama import *
-list_users = convertuser("user.csv")
-list_candi = convertcandi("candi.csv")
-list_bahan_bangunan = convertbb("bahan_bangunan.csv")
+from F09 import *
+
+userlist = convertuser("user.csv")
+candilist = convertcandi("candi.csv")
+bahanbangunanlist = convertbb("bahan_bangunan.csv")
+
+Username, Password, Role = None, None, None
+masukan = input(">>> ")
+while masukan != "exit":
+  
+  
+      # F09 - AMBIL LAPORAN JIN
+    if masukan == "laporanjin" and Role == "bandung_bondowoso":
+        laporan = getLaporan(list_users, list_bahan_bangunan, list_candi)
+        print(f"""
+> Total Jin: {laporan[0]}
+> Total Jin Pengumpul: {laporan[1]}  
+> Total Jin Pembangun: {laporan[2]}
+> Jin Terajin: {laporan[3]}
+> Jin Termalas: {laporan[4]}
+> Jumlah Pasir: {laporan[5]} unit
+> Jumlah Air: {laporan[6]} unit
+> Jumlah Batu : {laporan[7]} unit
+""")
+        masukan = input(">>> ")
+    elif masukan == "laporanjin" and Role != "bandung_bondowoso":
+        print("Laporan jin hanya dapat diakses oleh akun Bandung Bondowoso.")
+        masukan = input(">>> ")
