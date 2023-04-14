febb5b2f42e048e67ece0b23fc65d044958b6f51
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-15 01:12:44 +0700

    Create F09laporan.py
    F09 laporan

---
+++ F09laporan.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import math
+import time
+import argparse
+import datetime
+from typing import List
+
+
+from Fungsi2Utama import *
+
+def getLaporan(user : list, bahan : list, candi : list) -> tuple[int, int, int, str, str, str, str, str]:
+    jinpengumpul = 0; jinpembangun = 0; topterajin = "-"; toptermalas = "-"; pasir = 0; batu = 0; air = 0
+    
+    # menghitung batu pasir air
+    for i in range(Len(bahan)):
+        if bahan[i].nama == "batu":
+            batu = bahan[i].jumlah
+        elif bahan[i].nama == "pasir":
+            pasir = bahan[i].jumlah
+        else:
+            air = bahan[i].jumlah
+    
+    # menghitung jin jin
+    for i in range(2, Len(user)):
+        if user[i].role == "jin_pengumpul":
+            jinpengumpul += 1
+        else:
+            jinpembangun += 1
+    
+    if jinpembangun != 0:
+        # menghitung anggota anggota jin nya
+        # memasukkan jin jin ke array
+        jinlist = [None for i in range(NMax)]; jinlist[0] = Mark
+        for i in range(2, Len(user)):
+            if user[i].role == "jin_pembangun":
+                Append(jinlist, user[i].username)
+
+        # declare jumlah2 berapa aja yang dibikin
+        jumlahdibangun = [0 for i in range(Len(jinlist)+2)]
+        jumlahdibangun[Len(jinlist)] = Mark; jumlahdibangun[Len(jinlist)+1] = None
+
+        # penghitungan 
+        for i in range(Len(candi)):
+            for j in range(Len(jinlist)):
+                if candi[i].pembuat == jinlist[j]:
+                    jumlahdibangun[j] += 1
+        
+        # ambil min dan max
+        min = Min(jumlahdibangun)
+        max = Max(jumlahdibangun)
+
+        # listing termalas dan terajin
+        termalas = [None for i in range(NMax)]; termalas[0] = Mark
+        for i in range(Len(jinlist)):
+            if jumlahdibangun[i] == min:
+                Append(termalas, jinlist[i])
+        
+        terajin = [None for i in range(NMax)]; terajin[0] = Mark
+        for i in range(Len(jinlist)):
+            if jumlahdibangun[i] == max:
+                Append(terajin, jinlist[i])
+
+        toptermalas = termalas[0]
+        for i in range(1, Len(termalas)):
+            toptermalas = CompareString(toptermalas, termalas[i])
+        
+        topterajin = terajin[0]
+        for i in range(1, Len(terajin)):
+            topterajin = CompareString(topterajin, terajin[i])
+
+        return [(jinpembangun+jinpengumpul), jinpengumpul, jinpembangun, topterajin, toptermalas, pasir, air, batu]
+    
+    else:
+        return [(jinpembangun+jinpengumpul), jinpengumpul, jinpembangun, "-", "-", pasir, air, batu]
