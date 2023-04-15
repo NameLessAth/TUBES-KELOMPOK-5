03bba93d34d52e5cceff9621beb0b1468d1d957f
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-15 22:16:46 +0700

    Penambahan F10 ke main.py

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import datetime
 
 from Fungsi-Utama import *
 from F09 import *
+from F10 import *
 
 userlist = convertuser("user.csv")
 candilist = convertcandi("candi.csv")
 bahanbangunanlist = convertbb("bahan_bangunan.csv")
 
 Username, Password, Role = None, None, None
-masukan = input(">>> ")
-while masukan != "exit":
+perintah = input("Masukkan command yang akan dijalankan! : ")
+while perintah != "exit":
   
   
-      # F09 - AMBIL LAPORAN JIN
+    # F09 - AMBIL LAPORAN JIN
     if masukan == "laporanjin" and Role == "bandung_bondowoso":
-        laporan = getLaporan(list_users, list_bahan_bangunan, list_candi)
+        laporan = getLaporan(userlist, bahanbangunanlist, candilist)
         print(f"""
 > Total Jin: {laporan[0]}
 > Total Jin Pengumpul: {laporan[1]}  
@@ -30,7 +31,39 @@ while masukan != "exit":
 > Jumlah Air: {laporan[6]} unit
 > Jumlah Batu : {laporan[7]} unit
 """)
-        masukan = input(">>> ")
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+    # KETIKA BUKAN BANDUNG BONDOWOSO YANG MENGAKSES
     elif masukan == "laporanjin" and Role != "bandung_bondowoso":
         print("Laporan jin hanya dapat diakses oleh akun Bandung Bondowoso.")
-        masukan = input(">>> ")
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+        
+        
+    
+    # F10 - AMBIL LAPORAN CANDI
+    if perintah == "laporancandi" and Role == "bandung_bondowoso":
+        laporan = LaporanCandi(list_candi)
+
+        # printing ketika ada setidaknya 1 candi
+        if laporan[0] != 0:
+            print(f"""
+> Total Candi: {laporan[0]}
+> Total Pasir yang digunakan: {laporan[1]}
+> Total Batu yang digunakan: {laporan[2]}
+> Total Air yang digunakan: {laporan[3]}
+> ID Candi Termahal: {laporan[4]} (Rp {laporan[5]})
+> ID Candi Termurah: {laporan[6]} (Rp {laporan[7]})""")
+        
+        # printing ketika tidak ada candi sama sekali
+        else:
+            print(f"""
+> Total Candi: {laporan[0]}
+> Total Pasir yang digunakan: {laporan[1]}
+> Total Batu yang digunakan: {laporan[2]}
+> Total Air yang digunakan: {laporan[3]}
+> ID Candi Termahal: -
+> ID Candi Termurah: -""")
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+    # KETIKA BUKAN BANDUNG BONDOWOSO YANG MENGAKSES
+    elif perintah == "laporancandi" and Role != "bandung_bondowoso":
+        print("Laporan candi hanya dapat diakses oleh akun Bandung Bondowoso.")
+        perintah = input("Masukkan command yang akan dijalankan! : ")
