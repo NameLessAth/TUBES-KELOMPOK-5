f276ab206e57369e7cf6489e2851a720260dd8c5
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-17 08:08:02 +0700

    Update main.py
    Update main.py menambahkan F01, F02, F09, F10, F11, F12

--- main.py
+++ main.py
@@ -5,65 +5,69 @@ import time
 import argparse
 import datetime
 
-from Fungsi-Utama import *
+from FungsiUtama import *
+from F01 import *
+from F02 import *
 from F09 import *
 from F10 import *
+from F11 import *
+from F12 import *
 
 userlist = convertuser("user.csv")
 candilist = convertcandi("candi.csv")
 bahanbangunanlist = convertbb("bahan_bangunan.csv")
 
-Username, Password, Role = None, None, None
+Username, Role = None, None
 perintah = input("Masukkan command yang akan dijalankan! : ")
 while perintah != "exit":
-  
-  
-    # F09 - AMBIL LAPORAN JIN
-    if masukan == "laporanjin" and Role == "bandung_bondowoso":
-        laporan = getLaporan(userlist, bahanbangunanlist, candilist)
-        print(f"""
-> Total Jin: {laporan[0]}
-> Total Jin Pengumpul: {laporan[1]}  
-> Total Jin Pembangun: {laporan[2]}
-> Jin Terajin: {laporan[3]}
-> Jin Termalas: {laporan[4]}
-> Jumlah Pasir: {laporan[5]} unit
-> Jumlah Air: {laporan[6]} unit
-> Jumlah Batu : {laporan[7]} unit
-""")
+
+    # F01 - LOGIN
+    if perintah == "login":
+        Username, Role = Login(userlist, Role, Username)
         perintah = input("Masukkan command yang akan dijalankan! : ")
-    # KETIKA BUKAN BANDUNG BONDOWOSO YANG MENGAKSES
-    elif masukan == "laporanjin" and Role != "bandung_bondowoso":
+    
+    # F?? - MINIMAL LOGIN
+    if Role == None:
+        print("Kamu Belum login!")
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+
+    # F02 - LOGOUT
+    if perintah == "logout":
+        Username, Role = logout(Username, Role)
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+    
+    
+    # F09 -  AMBIL LAPORAN JIN
+    if perintah == "laporanjin" and Role == "bandung_bondowoso":
+        LaporanJin(userlist, bahanbangunanlist, candilist)
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+    elif perintah == "laporanjin" and Role != "bandung_bondowoso":
         print("Laporan jin hanya dapat diakses oleh akun Bandung Bondowoso.")
         perintah = input("Masukkan command yang akan dijalankan! : ")
-        
-        
-    
+
     # F10 - AMBIL LAPORAN CANDI
     if perintah == "laporancandi" and Role == "bandung_bondowoso":
-        laporan = LaporanCandi(list_candi)
-
-        # printing ketika ada setidaknya 1 candi
-        if laporan[0] != 0:
-            print(f"""
-> Total Candi: {laporan[0]}
-> Total Pasir yang digunakan: {laporan[1]}
-> Total Batu yang digunakan: {laporan[2]}
-> Total Air yang digunakan: {laporan[3]}
-> ID Candi Termahal: {laporan[4]} (Rp {laporan[5]})
-> ID Candi Termurah: {laporan[6]} (Rp {laporan[7]})""")
-        
-        # printing ketika tidak ada candi sama sekali
-        else:
-            print(f"""
-> Total Candi: {laporan[0]}
-> Total Pasir yang digunakan: {laporan[1]}
-> Total Batu yang digunakan: {laporan[2]}
-> Total Air yang digunakan: {laporan[3]}
-> ID Candi Termahal: -
-> ID Candi Termurah: -""")
+        LaporanCandi(candilist)
         perintah = input("Masukkan command yang akan dijalankan! : ")
-    # KETIKA BUKAN BANDUNG BONDOWOSO YANG MENGAKSES
+    
     elif perintah == "laporancandi" and Role != "bandung_bondowoso":
         print("Laporan candi hanya dapat diakses oleh akun Bandung Bondowoso.")
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+
+    # F11 - HANCURKAN CANDI
+    if perintah == "hancurkancandi" and Role == "roro_jonggrang":
+        candilist = HancurkanCandi(candilist)
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+    
+    elif perintah == "hancurkancandi" and Role != "roro_jonggrang":
+        print("Hancurkan candi hanya bisa dilakukan oleh akun Roro Jonggrang!")
+        perintah = input("Masukkan command yang akan dijalankan! : ")
+
+    # F12 - AYAM BERKOKOK
+    if perintah == "ayamberkokok" and Role == "roro_jonggrang":
+        AyamBerkokok(candilist)
+        perintah = "exit"
+        
+    elif perintah == "ayamberkokok" and Role != "roro_jonggrang":
+        print("Ayam Berkokok hanya bisa dilakukan oleh akun Roro Jonggrang")
         perintah = input("Masukkan command yang akan dijalankan! : ")
