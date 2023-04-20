56360ea3bd478bef5f7415051de90b69402d0944
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-21 02:09:02 +0700

    Create F04hapusjin.py

---
+++ F04hapusjin.py
@@ -0,0 +1,55 @@
+from FungsiUtama import *
+
+def hapusJin(userlist : list, candilist : list, role : str) -> tuple[list, list]:
+    # validasi yang mengakses bandung
+    if role == "bandung_bondowoso":
+
+        # input username jin 
+        uname = input("Masukkan username jin: ")
+
+        # mencari uname dari jin
+        i = 0; j=0
+        while (i < NMax):
+
+            # bila tidak ketemu sama sekali
+            if userlist[i] == Mark:
+                print("\nTidak ada jin dengan username tersebut!")
+                return userlist, candilist
+            
+            # mencari
+            elif userlist[i] != None:
+
+                # bila ketemu
+                if userlist[i].username == uname:
+
+                    # verifikasi
+                    if (input(f"Apakah anda yakin ingin menghapus jin dengan username {uname} (Y/N)? ") == "Y"):
+                        userlist[i] = None
+                        print("\nJin telah berhasil dihapus dari alam gaib.")
+
+                        # mencari candi yang telah dibuat oleh jin tersebut
+                        while (j < NMax):
+                            
+                            # return
+                            if candilist[j] == Mark:
+                                return userlist, candilist
+                            
+                            elif candilist[j] != None:
+                                # bila ada yang dibuat candi oleh jin nya
+                                if candilist[j].pembuat == uname:
+                                    candilist[j] = None
+                            j += 1
+                    
+                    # bila tidak jadi dihancurkan (verif == N)
+                    else:
+                        print("\ndipahami, semoga harimu menyenangkan, Bandung!")
+                        return userlist, candilist
+            i += 1
+
+    else:
+
+        print("Perintah hanya dapat diakses oleh akun Bandung Bondowoso.")
+        return userlist, candilist
+                            
+                    
+
