053b9cdbf17e1ea066d19297f2f6a752895e7c52
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-21 02:21:25 +0700

    Update F02logout.py

--- F02logout.py
+++ F02logout.py
@@ -1,23 +1,10 @@
-import os
-import sys
-import math
-import time
-import argparse
-import datetime
-from typing import List
-
-
 from FungsiUtama import *
 
-def logout(Username : str, Role : str) -> tuple[str, str]:
-    
-    # bila logout dan belum login
+def Logout(Username : str, Role : str) -> tuple[str, str]:
     if (Role == None):
         print("Logout gagal!")
         print("Anda belum login, silakan login terlebih dahulu!")
         return Username, Role
-    
-    # logout dengan lancar
     else:
         print("Terima kasih sudah bermain, sampai jumpa di lain hari!")
         return None, None
