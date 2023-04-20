555bada5c6dcbe3ffb8e08ee46df1d2bc7323b2d
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-21 02:13:46 +0700

    Update F11hancurkan.py

--- F11hancurkan.py
+++ F11hancurkan.py
@@ -1,39 +1,36 @@
-import os
-import sys
-import math
-import time
-import argparse
-import datetime
-from typing import List
-
-
 from FungsiUtama import *
 
-def HancurkanCandi (candi : list) -> list:
-    # input ID Candi yang hendak dihancurkan
-    IDCandi = int(input("Masukkan ID candi: ")) 
+def HancurkanCandi (candi : list, Role : str) -> list:
+    if Role == "roro_jonggrang":
+        # input ID Candi yang hendak dihancurkan
+        IDCandi = int(input("Masukkan ID candi: ")) 
 
-    # pencarian ID yang telah diinput
-    for i in range(Len(candi)):
-        if candi[i] != None:
+        # pencarian ID yang telah diinput
+        for i in range(Len(candi)):
+            if candi[i] != None:
 
-            # bila ID ditemukan
-            if candi[i].id == IDCandi:
+                # bila ID ditemukan
+                if candi[i].id == IDCandi:
 
-                # meminta argumen ya atau tidak
-                Hancur = input(f"Apakah anda yakin ingin menghancurkan candi ID: {IDCandi} (Y/N)? ")
+                    # meminta argumen ya atau tidak
+                    Hancur = input(f"Apakah anda yakin ingin menghancurkan candi ID: {IDCandi} (Y/N)? ")
 
-                # argumen Y, candi dibuat None dan disimpan listnya
-                if Hancur == "Y":
-                    candi[i] = None
-                    print("\nCandi berhasil dihancurkan")
-                    return candi
-                
-                # argumen N, candi tidak jadi dihancurkan
-                else:
-                    print("Candi tidak jadi dihancurkan")
-                    return candi
-                
-    # bila tidak ditemukan ID yang dicari
-    print("\nTidak ada candi dengan ID tersebut!")
+                    # argumen Y, candi dibuat None dan disimpan listnya
+                    if Hancur == "Y":
+                        candi[i] = None
+                        print("\nCandi berhasil dihancurkan")
+                        return candi
+                    
+                    # argumen N, candi tidak jadi dihancurkan
+                    else:
+                        print("Candi tidak jadi dihancurkan")
+                        return candi
+                    
+        # bila tidak ditemukan ID yang dicari
+        print("\nTidak ada candi dengan ID tersebut!")
+        
+    # bila yang mengakses bukan Roro Jonggrang    
+    else:
+        print("Hancurkan candi hanya bisa dilakukan oleh akun Roro Jonggrang!")
+    # return fungsi
     return candi
