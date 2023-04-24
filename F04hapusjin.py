e8c164de991b5b872599537a23aa91c723345668
NameLess <119790212+NameLessAth@users.noreply.github.com>
2023-04-24 11:08:45 +0700

    Update F04hapusjin.py

--- F04hapusjin.py
+++ F04hapusjin.py
@@ -1,6 +1,6 @@
 from FungsiUtama import *
 
-def hapusJin(userlist : list, candilist : list, role : str) -> tuple[list, list]:
+def hapusJin(userlist : list, candilist : list, stackUndo : list, role : str) -> tuple[list, list, list]:
     # validasi yang mengakses bandung
     if role == "bandung_bondowoso":
 
@@ -14,7 +14,7 @@ def hapusJin(userlist : list, candilist : list, role : str) -> tuple[list, list]
             # bila tidak ketemu sama sekali
             if userlist[i] == Mark:
                 print("\nTidak ada jin dengan username tersebut!")
-                return userlist, candilist
+                return userlist, candilist, stackUndo
             
             # mencari
             elif userlist[i] != None:
@@ -24,6 +24,10 @@ def hapusJin(userlist : list, candilist : list, role : str) -> tuple[list, list]
 
                     # verifikasi
                     if (input(f"Apakah anda yakin ingin menghapus jin dengan username {uname} (Y/N)? ") == "Y"):
+                        
+                        userUndo = userlist[i]
+                        candiUndo = [None for i in range(NMax)]; candiUndo[0] = Mark
+
                         userlist[i] = None
                         print("\nJin telah berhasil dihapus dari alam gaib.")
 
@@ -32,24 +36,22 @@ def hapusJin(userlist : list, candilist : list, role : str) -> tuple[list, list]
                             
                             # return
                             if candilist[j] == Mark:
-                                return userlist, candilist
+                                return userlist, candilist, Append(stackUndo, [userUndo, candiUndo])
                             
                             elif candilist[j] != None:
                                 # bila ada yang dibuat candi oleh jin nya
                                 if candilist[j].pembuat == uname:
+                                    Append(candiUndo, candilist[j])
                                     candilist[j] = None
                             j += 1
                     
                     # bila tidak jadi dihancurkan (verif == N)
                     else:
                         print("\ndipahami, semoga harimu menyenangkan, Bandung!")
-                        return userlist, candilist
+                        return userlist, candilist, stackUndo
             i += 1
 
     else:
 
         print("Perintah hanya dapat diakses oleh akun Bandung Bondowoso.")
-        return userlist, candilist
-                            
-                    
-
+        return userlist, candilist, stackUndo
